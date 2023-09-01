d4d2f5e75b5aa34af2e503d03a4adce5238e23c6
Your Name <youremail@example.com>
2023-09-01 19:59:26 +0300

    commit message

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,10 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+     def __unit__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = []
+
+     def learn(self, subject):
+        self.knowledge.append(subject)
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python
-
-from user import User
-
 import random
-
+from user import User
 class Teacher(User):
+    def __init__(self, first_name, last_name, knowledge):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
+
+     
+     # The knowledge list provided
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
+# Create a Teacher instance with the provided knowledge
+knowledge = "Mathematics"
+teacher = Teacher("John", "Doe", knowledge)
+
+# Access attributes
+print(teacher.first_name)    # Output: John
+print(teacher.last_name)     # Output: Doe
+print(teacher.knowledge)     # Output: The list of knowledge strings
